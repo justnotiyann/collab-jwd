265291063ea86d7692e305a6d8e59ff03cb1762b
justnotiyann <muhammadfitrian0712@gmail.com>
2022-08-24 17:48:25 +0700

    add remote database

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("collab_jwd", "root", "", {
-  host: "localhost",
+const db = new Sequelize("freedb_collab_jwd", "freedb_iyanxdev", "2!hcu5%8&GbqKj#", {
+  host: "sql.freedb.tech",
+  port: 3306,
   dialect: "mysql",
 });
 
@@ -9,7 +10,7 @@ try {
   db.authenticate();
   console.log("Koneksi database berhasil");
 } catch (error) {
-  console.log(error);
+  console.log(error.message);
 }
 
 module.exports = db;
